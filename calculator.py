5fc26c8f83e5c3231dfe81b539125400611b7ba0
xavidosil0-svg <xavidosil0@gmail.com>
2026-07-26 16:43:01 -0400

    changed nothing

---
+++ calculator.py
@@ -0,0 +1,24 @@
+# x=int(input("1st num"))
+# y=int(input("2nd num"))
+# print((x+y)**2)
+#x=7
+#y=8
+#f=7==8
+#print(f)
+#e=True
+#if e:
+ #   print("e is true")
+#else:
+ #   print ("e is not true")
+x=int(input("1st num"))
+y=int(input("2nd num"))
+if x<y:
+    print ("x<y")
+if x>y:
+    print("x>y")
+if x>=y:
+    print ("x>=y")
+if x<=y:
+    print ("x<=y")
+if x==y:
+    print ("x==y")
---
+++ test.py
@@ -0,0 +1,3 @@
+noun1 = input ("put a noun in")
+adjective1 =  input ("put an adjective in")
+print ("He", adjective1, "to", noun1)
