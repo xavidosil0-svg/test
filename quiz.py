5bd569c4784e4ad0fa2d8448d38d35102f5a5be2
xavidosil0-svg <xavidosil0@gmail.com>
2026-07-26 16:56:06 -0400

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,21 @@
+print("Whcih player scored the most goals?")
+print("A. Ronaldo")
+print("B. Messi")
+print("C. Kane")
+print("D. Yamal")
+answer=input()
+if answer=="B":
+    print("Correct")
+else:
+    print ("Incorrect")
+
+print("Whcih league ia better?")
+print("A. EPL")
+print("B. LL")
+print("C. Ligue 1")
+print("D. MLS")
+answer=input()
+if answer=="B":
+    print("Correct")
+else:
+    print ("Incorrect")
