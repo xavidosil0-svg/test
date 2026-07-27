4f930f681de25fbe1cb31854e1549ea9eec77831
xavidosil0-svg <xavidosil0@gmail.com>
2026-07-27 11:05:27 -0400

    1105-10 mins left

---
+++ loop3.py
@@ -0,0 +1,7 @@
+x=0
+y=1
+for x in range (100):
+    print (x+y)
+    q=x+y
+    x=y
+    y=q
---
+++ loop4.py
@@ -0,0 +1,5 @@
+for j in range (3):
+    for i in range (3):
+        x=print("*", end="")
+    print()
+
