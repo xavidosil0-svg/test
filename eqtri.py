f6bcb4f723c9d052949c228aba269ba5a629feeb
xavidosil0-svg <xavidosil0@gmail.com>
2026-07-29 14:19:12 -0400

    committed 7/29

--- eqtri.py
+++ eqtri.py
@@ -1,5 +1,8 @@
-for j in range (6):
-    for i in range (5-j):
-        x=print(" ", end=" ")
-    for i in range (5+j):
-        print("*", end=" ")
+for j in range (9):
+    for i in range (9-j):
+        x=print("", end=" ")
+    for i in range (j+1):
+        print(" *", end=" ")
+    for i in range (j+12):
+        print(" ", end=" ")
+    print()
--- loops5.py
+++ loops5.py
@@ -1,4 +1,7 @@
-for j in range (4):
-    for i in range (j+1):
-        x=print("*", end="")
+for j in range (5):
+    for i in range (5):
+        if j==0 or j==4 or i==0 or i==4:
+            x=print("*", end="")
+        else:
+            print(" ", end="")
     print()
